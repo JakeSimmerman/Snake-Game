931028bb0d6b5964fa02489ec0e5ca1c50c5d315
Jake <jakessimmerman@gmail.com>
2020-07-22 01:16:03 -0500

    defined the redraw window class and the draw grid class to setup our table for the snake

--- snake.py
+++ snake.py
@@ -38,11 +38,23 @@ class snake(object):
 
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBetween = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBetween
+        y = y + sizeBetween
+
+        pygame.draw.line(surface, (255,255,255), (x, 0), (x,w))
+        pygame.draw.line(surface, (255,255,255), (0, y), (w,y))
 
 
 def redrawWindow(surface):
-    pass
+    global rows, width
+    surface.fill((0,0,0))
+    drawGrid(width, row, surface)
+    pygame.display.update()
 
 
 def randomSnack(rows, item):
@@ -54,6 +66,20 @@ def message_box(subject, content):
 
 
 def main():
+    global width, rows
+    width = 500
+    rows = 40
+    window = pygame.display.set_mode((width, width))
+    s = snake((255, 255, 0), (20, 20))
+    flag = True
+
+    clock = pygame.time.clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+
+        redrawWindow(window)
     pass
 
 
