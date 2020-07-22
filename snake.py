0e389301f875aeec0879c900df54fd9f4187516f
Jake <jakessimmerman@gmail.com>
2020-07-22 01:31:26 -0500

    created keys for turning

--- snake.py
+++ snake.py
@@ -22,14 +22,36 @@ class snake(object):
     body = []
     turns = {}
     def __init__(self, color, pos):
-        slef.color = color
+        self.color = color
         self.head = cube(pos)
         self.body.append(self, head)
         self.dirnx = 0
         self.dirny = 1
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                if keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                if keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                if keys.[pygame.K_down]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
 
     def reset(self, pos):
